418b6c23239c2c516cbfa4b5d389133b776775c1
alfredclark <alfredclark@163.com>
2021-02-09 22:27:32 +0800

    2021/2/9

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& A, int K) {
+        int len=A.size(), size=0;
+        vector<int> counter(len + 1);
+        int dp=1;
+        int last=0, res=0;
+        for(int i:A) {
+            size+=counter[i]++>0?0:1;
+            if(size>K) {
+                counter[A[last++]]=0;
+                size--;
+                dp=1;
+            }
+            if(size==K) {
+                while(counter[A[last]]>1) {
+                    counter[A[last++]]--;
+                    dp++;
+                }
+                res+=dp;
+            }
+        }
+        return res;
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Solution solution;
+    int nums[] = {1, 2, 3, 2, 1};
+    vector<int> A(nums, nums + sizeof(nums) / sizeof (int));
+    cout << solution.subarraysWithKDistinct(A, 2);
     return 0;
 }
