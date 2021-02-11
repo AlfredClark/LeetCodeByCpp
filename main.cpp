443b71ed836be7c00f86a2d48cbae876caa67707
alfredclark <alfredclark@163.com>
2021-02-11 16:10:44 +0800

    2021/2/11

--- main.cpp
+++ main.cpp
@@ -2,34 +2,44 @@
 
 using namespace std;
 
-class Solution {
+class KthLargest {
 public:
-    bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length())
-            return false;
-        vector<short> count1(26, 0);
-        vector<short> count2(26, 0);
-        for (char s: s1)
-            count1[s - 'a']++;
-        int len = s1.length();
-        for (int i = 0; i < len; ++i)
-            count2[s2[i] - 'a']++;
-        if (count1 == count2)
-            return true;
-        for (int i = len; i < s2.length(); ++i) {
-            count2[s2[i] - 'a']++;
-            count2[s2[i - len] - 'a']--;
-            if (count1 == count2)
-                return true;
+    int k;
+    priority_queue<int, vector<int>, greater<>> pq;
+
+    KthLargest(int k, vector<int> &nums) {
+        this->k = k;
+        for (int num: nums)
+            pq.push(num);
+        while (pq.size() > k)
+            pq.pop();
+    }
+
+    int add(int val) {
+        if (pq.size() < k)
+            pq.push(val);
+        else if (val > pq.top()) {
+            pq.push(val);
+            pq.pop();
         }
-        return false;
+        return pq.top();
     }
 };
 
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+
 int main() {
-    Solution solution;
-    string s1 = "ooabo";
-    string s2 = "eidbaooo";
-    cout << solution.checkInclusion(s1, s2);
+    int k = 3;
+    int num[] = {};
+    int test[] = {3, 5, 10, 9, 4};
+    vector<int> nums(num, num + sizeof(num) / sizeof(int));
+    auto *obj = new KthLargest(k, nums);
+    for (int i : test) {
+        cout << obj->add(i) << " ";
+    }
     return 0;
 }
