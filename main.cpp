2b916ac86c8b020521ea48f2f24fd2688429fb84
alfredclark <alfredclark@163.com>
2021-02-18 05:45:20 +0800

    2021/2/18

--- main.cpp
+++ main.cpp
@@ -4,19 +4,31 @@ using namespace std;
 
 class Solution {
 public:
-    int arrayPairSum(vector<int> &nums) {
-        int sum = 0;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i += 2)
-            sum += nums[i];
-        return sum;
+    queue<int> q;
+    int minKBitFlips(vector<int> &A, int K) {
+        int n = A.size();
+        int res = 0, p = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i == q.front()){
+                q.pop();
+                p = !p;
+            }
+            if (A[i] == p){
+                if (i + K > n)
+                    return -1;
+                ++res;
+                q.push(i + K);
+                p = !p;
+            }
+        }
+        return res;
     }
 };
 
 int main() {
     Solution solution;
-    int num[] = {1, 3, 5, 8, 23, 63, 11, 9, 3, 2, 66, 23};
+    int num[] = {0, 0, 0, 1, 0, 1, 1, 0};
     vector<int> row(num, num + sizeof(num) / sizeof(int));
-    cout << solution.arrayPairSum(row);
+    cout << solution.minKBitFlips(row, 3);
     return 0;
 }
