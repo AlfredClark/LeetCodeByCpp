30eed7bae3ed6962c5ca60de121eb8d699ef1874
alfredclark <alfredclark@163.com>
2021-03-02 09:56:05 +0800

    2021/3/2

--- main.cpp
+++ main.cpp
@@ -2,27 +2,38 @@
 
 using namespace std;
 
-class NumArray {
+class NumMatrix {
 public:
-    int *sums;
+    vector<vector<int>> sums;
 
-    NumArray(vector<int> &nums) {
-        int n = nums.size();
-        sums = new int[n + 1];
-        for (int i = 0; i < n; ++i)
-            sums[i + 1] = sums[i] + nums[i];
+    NumMatrix(vector<vector<int>> &matrix) {
+        int m = matrix.size();
+        if (m > 0) {
+            int n = matrix[0].size();
+            sums.resize(m + 1, vector<int>(n + 1));
+            for (int i = 0; i < m; ++i)
+                for (int j = 0; j < n; ++j)
+                    sums[i + 1][j + 1] = sums[i][j + 1] + sums[i + 1][j] - sums[i][j] + matrix[i][j];
+        }
     }
 
-    int sumRange(int i, int j) {
-        return sums[j + 1] - sums[i];
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
     }
 };
 
 int main() {
-    int num[] = {-2, 0, 3, -5, 2, -1};
-    vector<int> nums(num, num + sizeof(num) / sizeof(int));
-    NumArray *obj = new NumArray(nums);
-    int param_1 = obj->sumRange(2, 5);
-    cout << param_1 << endl;
+    int m[5][5] = {
+            {3, 0, 1, 4, 2},
+            {5, 6, 3, 2, 1},
+            {1, 2, 0, 1, 5},
+            {4, 1, 0, 1, 7},
+            {1, 0, 3, 0, 5}
+    };
+    vector<vector<int>> matrix;
+    for (auto &i : m)
+        matrix.emplace_back(i, i + 5);
+    NumMatrix numMatrix(matrix);
+    cout << numMatrix.sumRegion(1, 1, 2, 2);
     return 0;
 }
