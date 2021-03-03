1ba6430da75fe136adcf2e120c7728f0042ea23c
alfredclark <alfredclark@163.com>
2021-03-03 09:41:02 +0800

    2021/3/3

--- main.cpp
+++ main.cpp
@@ -2,38 +2,19 @@
 
 using namespace std;
 
-class NumMatrix {
+class Solution {
 public:
-    vector<vector<int>> sums;
-
-    NumMatrix(vector<vector<int>> &matrix) {
-        int m = matrix.size();
-        if (m > 0) {
-            int n = matrix[0].size();
-            sums.resize(m + 1, vector<int>(n + 1));
-            for (int i = 0; i < m; ++i)
-                for (int j = 0; j < n; ++j)
-                    sums[i + 1][j + 1] = sums[i][j + 1] + sums[i + 1][j] - sums[i][j] + matrix[i][j];
-        }
-    }
-
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
+    vector<int> countBits(int num) {
+        vector<int> result(num + 1);
+        for (int i = 0; i <= num; ++i)
+            result[i] = bitset<64>(i).count();
+        return result;
     }
 };
 
 int main() {
-    int m[5][5] = {
-            {3, 0, 1, 4, 2},
-            {5, 6, 3, 2, 1},
-            {1, 2, 0, 1, 5},
-            {4, 1, 0, 1, 7},
-            {1, 0, 3, 0, 5}
-    };
-    vector<vector<int>> matrix;
-    for (auto &i : m)
-        matrix.emplace_back(i, i + 5);
-    NumMatrix numMatrix(matrix);
-    cout << numMatrix.sumRegion(1, 1, 2, 2);
+    Solution solution;
+    for (int num:solution.countBits(100))
+        cout << num << "\t";
     return 0;
 }
