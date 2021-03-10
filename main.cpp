2347a424b7480a7fb4f2aa01349873311be91e6c
alfredclark <alfredclark@163.com>
2021-03-10 21:58:31 +0800

    2021/3/10

--- main.cpp
+++ main.cpp
@@ -4,20 +4,37 @@ using namespace std;
 
 class Solution {
 public:
-    string removeDuplicates(string S) {
-
+    int calculate(const string &s) {
+        int ret = 0, base = 1, i = 0, n = s.size();
+        stack<short> pos;
+        pos.push(1);
+        while (i < n) {
+            char c = s[i];
+            if (c == '+') {
+                base = pos.top();
+            } else if (c == '-') {
+                base = -pos.top();
+            } else if (c == '(') {
+                pos.push(base);
+            } else if (c == ')') {
+                pos.pop();
+            } else if (c != ' ') {
+                pos.push(i);
+                while (i < n && isdigit(s[i + 1]))
+                    i++;
+                ret += base * atoi(s.substr(pos.top(), i - pos.top() + 1).c_str());
+                pos.pop();
+            }
+            i++;
+        }
+        return ret;
     }
 };
 
 int main() {
     Solution solution;
-    cout << solution.removeDuplicates("asdfghjjhgftgb");
-//    string s = "aabaaccbdbc";
-//    for (const auto &res: solution.partition(s)) {
-//        for (const auto &r: res)
-//            cout << r << "\t";
-//        cout << endl;
-//    }
+    string s = "(1+(4+5+2)-3)+(6+8)";
+    cout << solution.calculate(s) << endl;
     return 0;
 }
 
