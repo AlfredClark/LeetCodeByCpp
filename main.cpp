681ce80618887927d378d0ba556980fe96d43094
alfredclark <alfredclark@163.com>
2021-03-12 19:37:33 +0800

    2021/3/12

--- main.cpp
+++ main.cpp
@@ -3,47 +3,26 @@
 using namespace std;
 
 class Solution {
-private:
-    static void nums_opt(vector<int> &nums, char opt, int num) {
-        switch (opt) {
-            case '+':
-                nums.push_back(num);
-                break;
-            case '-':
-                nums.push_back(-num);
-                break;
-            case '*':
-                nums.back() *= num;
-                break;
-            default:
-                nums.back() /= num;
-        }
-    }
-
 public:
-    int calculate(string s) {
-        vector<int> nums;
-        int num = 0, n = s.size();
-        char last_opt = '+';
-        for (int i = 0; i < n; ++i) {
-            char c = s[i];
-            if (isdigit(c)) {
-                num = num * 10 + int(c - '0');
-            } else if (c != ' ') {
-                nums_opt(nums, last_opt, num);
-                last_opt = c;
-                num = 0;
-            }
-            if (i == n - 1)
-                nums_opt(nums, last_opt, num);
+    bool isValidSerialization(const string &preorder) {
+        int n = preorder.size();
+        int root = 1, i = -1;
+        while (++i < n) {
+            root += isdigit(preorder[i]) ? 1 : -1;
+            if (root < 1)
+                return i == n - 1;
+            while (i < n && preorder[i] != ',')
+                i++;
         }
-        return accumulate(nums.begin(), nums.end(), 0);
+        cout << root << endl;
+        return root == 0;
     }
 };
 
 int main() {
     Solution solution;
-    cout << solution.calculate(" 3/2") << endl;
+    string s = "9,3,4,#,#,1,#,#,2,#,6,#,#";
+    cout << solution.isValidSerialization(s) << endl;
     return 0;
 }
 
