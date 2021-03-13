e59070137578ac4eed82f8b2e948658101dc2586
alfredclark <alfredclark@163.com>
2021-03-13 10:39:44 +0800

    2021/3/13

--- main.cpp
+++ main.cpp
@@ -2,27 +2,44 @@
 
 using namespace std;
 
-class Solution {
+class MyHashSet {
+private:
+    int parm;
+    vector<list<int>> hashMap;
+
+    int hash(int key) {
+        return key % parm;
+    }
+
 public:
-    bool isValidSerialization(const string &preorder) {
-        int n = preorder.size();
-        int root = 1, i = -1;
-        while (++i < n) {
-            root += isdigit(preorder[i]) ? 1 : -1;
-            if (root < 1)
-                return i == n - 1;
-            while (i < n && preorder[i] != ',')
-                i++;
-        }
-        cout << root << endl;
-        return root == 0;
+    MyHashSet() {
+        parm = 100;
+        hashMap.resize(parm);
+    }
+
+    void add(int key) {
+        hashMap[hash(key)].push_back(key);
+    }
+
+    void remove(int key) {
+        hashMap[hash(key)].remove(key);
+    }
+
+    bool contains(int key) {
+        return find(hashMap[hash(key)].begin(), hashMap[hash(key)].end(), key) != hashMap[hash(key)].end();
     }
 };
 
 int main() {
-    Solution solution;
-    string s = "9,3,4,#,#,1,#,#,2,#,6,#,#";
-    cout << solution.isValidSerialization(s) << endl;
+    MyHashSet *obj = new MyHashSet();
+    obj->add(1);
+    obj->add(3);
+    cout << obj->contains(1) << endl;
+    cout << obj->contains(3) << endl;
+    obj->remove(1);
+    obj->remove(3);
+    cout << obj->contains(1) << endl;
+    cout << obj->contains(3) << endl;
     return 0;
 }
 
