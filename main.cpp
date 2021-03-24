662c707f0fae4d52014f5c56ed8787e073064d59
alfredclark <alfredclark@163.com>
2021-03-24 20:41:13 +0800

    2021/3/24

--- main.cpp
+++ main.cpp
@@ -4,14 +4,27 @@ using namespace std;
 
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        return bitset<32>(n).count();
+    bool find132pattern(vector<int> &nums) {
+        stack<int> stk;
+        int med = INT_MIN;
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            if (nums[i] < med)
+                return true;
+            while (!stk.empty() && stk.top() < nums[i]) {
+                med = max(stk.top(), med);
+                stk.pop();
+            }
+            stk.push(nums[i]);
+        }
+        return false;
     }
 };
 
 int main() {
     Solution solution;
-    cout << solution.hammingWeight(00101101) << endl;
+    int num[] = {-1, 3, 2, 0};
+    vector<int> nums(num, num + sizeof(num) / sizeof(int));
+    cout << solution.find132pattern(nums) << endl;
     return 0;
 }
 
