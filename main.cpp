011739393a76d58312d23bb7920a5f92bc33dcf8
alfredclark <alfredclark@163.com>
2021-03-30 18:29:44 +0800

    2021/3/30

--- main.cpp
+++ main.cpp
@@ -2,41 +2,19 @@
 
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-
-    ListNode() : val(0), next(nullptr) {}
-
-    ListNode(int x) : val(x), next(nullptr) {}
-
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
 class Solution {
 public:
-    ListNode *deleteDuplicates(ListNode *head) {
-        if (!head)
-            return head;
-        int val = head->val;
-        ListNode *p = head;
-        while (p->next) {
-            if (p->next->val == val) {
-                ListNode *d = p->next;
-                p->next = p->next->next;
-                delete (d);
-            } else {
-                val = p->next->val;
-                p = p->next;
-            }
-        }
-        return head;
+    uint32_t reverseBits(uint32_t n) {
+        bitset<32> rev(n);
+        string str = rev.to_string();
+        reverse(str.begin(), str.end());
+        return bitset<32>(str).to_ulong();
     }
 };
 
 int main() {
     Solution solution;
-
+    cout << solution.reverseBits(43261596) << endl;
     return 0;
 }
 
