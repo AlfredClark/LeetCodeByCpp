bf4aa0ee2cb0edece6ad8e4032321985c91562e9
alfredclark <alfredclark@163.com>
2021-04-01 09:45:34 +0800

    2021/3/31

--- main.cpp
+++ main.cpp
@@ -4,17 +4,39 @@ using namespace std;
 
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
-        bitset<32> rev(n);
-        string str = rev.to_string();
-        reverse(str.begin(), str.end());
-        return bitset<32>(str).to_ulong();
+    vector<int> t;
+    vector<vector<int>> ans;
+
+    void dfs(bool choosePre, int cur, vector<int> &nums) {
+        if (cur == nums.size()) {
+            ans.push_back(t);
+            return;
+        }
+        dfs(false, cur + 1, nums);
+        if (!choosePre && cur > 0 && nums[cur - 1] == nums[cur]) {
+            return;
+        }
+        t.push_back(nums[cur]);
+        dfs(true, cur + 1, nums);
+        t.pop_back();
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        dfs(false, 0, nums);
+        return ans;
     }
 };
 
 int main() {
     Solution solution;
-    cout << solution.reverseBits(43261596) << endl;
+    int num[] = {1, 2, 5, 3, 4, 2, 5};
+    vector<int> nums(num, num + sizeof(num) / sizeof(int));
+    for (const auto &res:solution.subsetsWithDup(nums)) {
+        for (auto re:res)
+            cout << re << " ";
+        cout << endl;
+    }
     return 0;
 }
 
